ecf9a3f9786260e7c57c7a20dc54cc774b4b7abb
Leandro Martinez <lmartine@unicamp.br>
2023-07-26 10:27:36 -0300

    use TestItems

--- src/SPGBox.jl
+++ src/SPGBox.jl
@@ -1,11 +1,15 @@
 module SPGBox
 
+using TestItems
 using LinearAlgebra
+
+export spgbox!
+export spgbox
+export SPGBoxResult
 
 include("./SPGBoxResult.jl")
 include("./VAux.jl")
 include("./auxiliary_functions.jl")
 include("./spgbox_main.jl")
-export spgbox!, spgbox, SPGBoxResult
 
 end
